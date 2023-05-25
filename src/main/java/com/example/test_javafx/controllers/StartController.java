010ebcf95b08ffdb65c12ffc583ed4b1cbee0649
mustafa abu amna <109383573+Mostfaam@users.noreply.github.com>
2023-05-25 00:25:41 -0900

    Add files via upload

---
+++ src/main/java/com/example/test_javafx/controllers/StartController.java
@@ -0,0 +1,36 @@
+package com.example.test_javafx.controllers;
+
+import com.example.test_javafx.Navigation;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class StartController {
+
+    @FXML
+    public AnchorPane rootPane;
+
+    Navigation navigation = new Navigation();
+
+    public void onStudent() {
+        navigation.navigateTo(rootPane, navigation.STUDENT_FXML);
+    }
+
+
+
+    public void onReports(ActionEvent actionEvent) {
+
+    }
+
+    public void onLectuer(ActionEvent actionEvent) {
+        navigation.navigateTo(rootPane, navigation.CREAT_LECTUER);
+    }
+
+    public void onPresence(ActionEvent actionEvent) {
+        navigation.navigateTo(rootPane, navigation.ATTEND);
+
+    }
+    public void logout(ActionEvent actionEvent) {
+        navigation.navigateTo(rootPane,navigation.LOGOUT);
+    }
+}
